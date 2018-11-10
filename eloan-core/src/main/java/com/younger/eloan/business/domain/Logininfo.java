2522ffe6d3e1024745e80ac6d501325df897a552
youngerone <928942338@qq.com>
2018-11-10 23:44:41 +0800

    使用Mybatis插件逆向工程生成映射mapper文件

---
+++ eloan-core/src/main/java/com/younger/eloan/business/domain/Logininfo.java
@@ -0,0 +1,65 @@
+package com.younger.eloan.business.domain;
+
+import java.io.Serializable;
+
+public class Logininfo implements Serializable {
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private Byte state;
+
+    private Byte usertype;
+
+    private Boolean admin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Byte getState() {
+        return state;
+    }
+
+    public void setState(Byte state) {
+        this.state = state;
+    }
+
+    public Byte getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(Byte usertype) {
+        this.usertype = usertype;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
+    }
+}
